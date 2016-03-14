e164f72af57b1ef445cd71390a34e4a31e8c5c05
fengyouchao <fengyouchao@gmail.com>
2016-03-14 21:19:09 +0800

    Add shortcuts

--- src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserController.java
+++ src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserController.java
@@ -1,14 +1,17 @@
 package io.github.fengyouchao.seehttp.plugins.browser.controller;
 
-import io.github.fengyouchao.seehttp.plugins.browser.controller.BrowserTab;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -20,27 +23,46 @@ import java.util.ResourceBundle;
  */
 public class BrowserController implements Initializable {
 
-  @FXML
-  private TabPane tabPane;
+  @FXML private TabPane tabPane;
 
-  @FXML
-  private Tab newTab;
-  @FXML
-  private TextField locationFiled;
+  @FXML private Tab newTab;
+  @FXML private TextField locationFiled;
   private int index = 0;
+  private boolean selectNextTabMode = false;
+  private boolean selectPreviousTabMode = false;
 
-  public void goBackAction(){
+  public void goBackAction() {
     getSelectTab().goBack();
   }
 
-  public void goForwardAction(){
+  public void goForwardAction() {
     getSelectTab().goForward();
   }
 
-  public void reloadAction(){
+  public void reloadAction() {
     getSelectTab().reload();
   }
 
+  public void handleKeyBoardShortcut(KeyEvent event) {
+    if (event.isControlDown() || event.isMetaDown()) {
+      if (event.getCode() == KeyCode.L) {
+        locationFiled.requestFocus();
+        locationFiled.selectAll();
+      } else if (event.getCode() == KeyCode.T) {
+        createNewTab();
+      } else if (event.getCode() == KeyCode.W) {
+        closeTab(getSelectTab());
+      } else if (event.getCode() == KeyCode.R) {
+        getSelectTab().reload();
+      } else if (event.getCode() == KeyCode.TAB) {
+        if (event.isShiftDown()) {
+          selectPreTab();
+        } else {
+          selectNextTab();
+        }
+      }
+    }
+  }
 
 
   @Override
@@ -56,28 +78,102 @@ public class BrowserController implements Initializable {
       }
       tab.go(url);
     });
-    tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
     newTab.selectedProperty().addListener((observable, oldValue, newValue) -> {
       if (newValue) {
         createNewTab();
+        locationFiled.requestFocus();
       }
     });
-    locationFiled.requestFocus();
+
+    tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
+    tabPane.setOnKeyPressed(event -> {
+      if (event.isControlDown() && event.getCode() == KeyCode.TAB) {
+        if (event.isShiftDown()) {
+          selectPreviousTabMode = true;
+        } else {
+          selectNextTabMode = true;
+        }
+      }
+      if (event.isConsumed()) {
+        selectNextTabMode = false;
+        selectPreviousTabMode = false;
+      }
+    });
+    tabPane.setOnKeyReleased(event -> {
+      selectNextTabMode = false;
+      selectPreviousTabMode = false;
+    });
+    tabPane.getSelectionModel().selectedIndexProperty()
+        .addListener((observable, oldValue, newValue) -> {
+          int index1 = newValue.intValue();
+          if (index1 == tabPane.getTabs().size() - 1) {
+            if (selectNextTabMode) {
+              index1 = 0;
+            }
+            if (selectPreviousTabMode) {
+              index1 = tabPane.getTabs().size() - 2;
+            }
+            tabPane.getSelectionModel().select(index1);
+          }
+        });
   }
 
   public BrowserTab createNewTab() {
     locationFiled.setText("");
-    locationFiled.setId(index+"");
+    locationFiled.setId((index++) + "");
     ObservableList<Tab> tabs = tabPane.getTabs();
     int index = tabs.size() - 1;
     BrowserTab tab = new BrowserTab(this);
     tabs.add(index, tab);
     tabPane.getSelectionModel().select(index);
-    locationFiled.requestFocus();
     return tab;
   }
 
-  public BrowserTab getSelectTab(){
+  public void selectNextTab() {
+    int selectIndex = tabPane.getSelectionModel().getSelectedIndex();
+    selectIndex++;
+    if (selectIndex == tabPane.getTabs().size() - 1) {
+      selectIndex = 0;
+    }
+    tabPane.getSelectionModel().select(selectIndex);
+  }
+
+  public void selectPreTab() {
+    int selectIndex = tabPane.getSelectionModel().getSelectedIndex();
+    selectIndex--;
+    if (selectIndex == 0) {
+      selectIndex = tabPane.getTabs().size() - 2;
+    }
+    tabPane.getSelectionModel().select(selectIndex);
+  }
+
+  public void closeTab(BrowserTab tab) {
+    int index = tabPane.getTabs().indexOf(tab);
+    tabPane.getTabs().remove(tab);
+    if (index == tabPane.getTabs().size() - 1) {
+      index--;
+      if (index < 0) {
+        index = 0;
+      }
+    }
+    tabPane.getSelectionModel().select(index);
+  }
+
+  public void closeOtherTab(BrowserTab keepTab) {
+    int index = tabPane.getTabs().indexOf(keepTab);
+    List<Tab> removedTabs = new ArrayList<>();
+    for (int i = 0; i < tabPane.getTabs().size() - 1; i++) {
+      if (i != index) {
+        removedTabs.add(tabPane.getTabs().get(i));
+      }
+    }
+    for (Tab tab : removedTabs) {
+      tabPane.getTabs().remove(tab);
+    }
+  }
+
+
+  public BrowserTab getSelectTab() {
     return (BrowserTab) tabPane.getSelectionModel().getSelectedItem();
   }
 
--- src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserTab.java
+++ src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserTab.java
@@ -1,7 +1,11 @@
 package io.github.fengyouchao.seehttp.plugins.browser.controller;
 
 import javafx.geometry.Pos;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.Tab;
+import javafx.scene.input.KeyCharacterCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
 import javafx.scene.web.WebEngine;
@@ -9,7 +13,7 @@ import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 
 /**
- * The class <code></code>
+ * The class <code>BrowserTab</code> represents tab of browser.
  *
  * @author Youchao Feng
  * @version 1.0
@@ -22,6 +26,11 @@ public class BrowserTab extends Tab {
   private String currentUrl = "";
   private BrowserController browserController;
   private Line progressLine = new Line();
+  private ContextMenu tabMenu;
+  private MenuItem newTabItem = new MenuItem("New Tab");
+  private MenuItem reloadItem = new MenuItem("Reload Page");
+  private MenuItem closeItem = new MenuItem("Close Tab");
+  private MenuItem closeOtherItem = new MenuItem("Close Other Tab");
 
   public BrowserTab(BrowserController browserController) {
     this.browserController = browserController;
@@ -55,6 +64,23 @@ public class BrowserTab extends Tab {
       double progress = newValue.doubleValue();
       progressLine.setEndX(stackPane.getWidth() * progress);
     });
+    initTabMenu();
+  }
+
+
+
+  private void initTabMenu(){
+    newTabItem.setAccelerator(new KeyCharacterCombination("Ctrl+T"));
+    newTabItem.setOnAction(event -> getBrowserController().createNewTab());
+    closeItem.setAccelerator(new KeyCharacterCombination("Ctrl+W"));
+    closeItem.setOnAction(event -> getBrowserController().closeTab(this));
+    closeOtherItem.setOnAction(event -> getBrowserController().closeOtherTab(this));
+    reloadItem.setAccelerator(new KeyCharacterCombination("Ctrl+R"));
+    reloadItem.setOnAction(event -> engine.reload());
+    tabMenu = new ContextMenu();
+    tabMenu.getItems().addAll(newTabItem,new SeparatorMenuItem(), reloadItem, new SeparatorMenuItem(), closeItem, closeOtherItem);
+    this.setContextMenu(tabMenu);
+    this.setClosable(true);
   }
 
   public WebView getWebView() {
@@ -78,9 +104,6 @@ public class BrowserTab extends Tab {
     System.out.println(history.getCurrentIndex());
     history.go(-1);
     System.out.println(history.getCurrentIndex());
-
-
-
   }
 
   public void goForward() {
@@ -95,5 +118,13 @@ public class BrowserTab extends Tab {
 
   public void setCurrentUrl(String currentUrl) {
     this.currentUrl = currentUrl;
+  }
+
+  public BrowserController getBrowserController() {
+    return browserController;
+  }
+
+  public void setBrowserController(BrowserController browserController) {
+    this.browserController = browserController;
   }
 }
