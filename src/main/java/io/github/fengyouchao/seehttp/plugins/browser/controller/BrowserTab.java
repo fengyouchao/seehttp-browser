1d084b8196c8eaa6b35e056a7aa19d4797f0f362
fengyouchao <fengyouchao@gmail.com>
2016-03-16 07:26:41 +0800

    add settings.fxml

--- src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserTab.java
+++ src/main/java/io/github/fengyouchao/seehttp/plugins/browser/controller/BrowserTab.java
@@ -1,15 +1,13 @@
 package io.github.fengyouchao.seehttp.plugins.browser.controller;
 
-import javafx.animation.StrokeTransition;
-import javafx.animation.Timeline;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Pos;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCharacterCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -17,7 +15,10 @@ import javafx.scene.shape.Line;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
-import javafx.util.Duration;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * The class <code>BrowserTab</code> represents tab of browser.
@@ -36,6 +37,7 @@ public class BrowserTab extends Tab {
   private ContextMenu tabMenu;
   private MenuItem newTabItem = new MenuItem("New Tab");
   private MenuItem reloadItem = new MenuItem("Reload Page");
+  private MenuItem copyItem = new MenuItem("Copy");
   private MenuItem closeItem = new MenuItem("Close Tab");
   private MenuItem closeOtherItem = new MenuItem("Close Other Tab");
   private Tooltip tooltip = new Tooltip();
@@ -45,52 +47,48 @@ public class BrowserTab extends Tab {
     this.setClosable(true);
     this.setText("New Tab");
     this.setTooltip(tooltip);
+    engine = webView.getEngine();
     StackPane stackPane = new StackPane();
+    stackPane.setAlignment(Pos.TOP_LEFT);
     stackPane.getChildren().addAll(webView, progressLine);
+    this.setContent(stackPane);
     progressLine.setLayoutX(0);
     progressLine.setLayoutY(0);
     progressLine.setStrokeWidth(2);
-    StrokeTransition strokeTransition = new StrokeTransition(Duration.seconds(3), progressLine,Color.DODGERBLUE, new Color(1.0,1.0,1.0,0.8));
-    strokeTransition.setCycleCount(0);
+    progressLine.setStroke(Color.DEEPSKYBLUE);
 
-    stackPane.setAlignment(Pos.TOP_LEFT);
-    this.setContent(stackPane);
-    engine = webView.getEngine();
     engine.locationProperty().addListener((observable, oldValue, newValue) -> {
       browserController.getLocationFiled().setText(newValue);
       currentUrl = newValue;
     });
-    tooltip.textProperty().bind(engine.titleProperty());
     engine.titleProperty().addListener((observable, oldValue, newValue) -> {
-      if(newValue!=null && !newValue.equalsIgnoreCase("")){
-        if(newValue.length()> 5){
+      if (newValue != null && !newValue.equalsIgnoreCase("")) {
+        if (newValue.length() > 5) {
           setText(newValue.substring(0, 5));
         }
       }
     });
 
     this.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if(newValue){
+      if (newValue) {
         browserController.getLocationFiled().setText(currentUrl);
       }
     });
     engine.setCreatePopupHandler(p -> browserController.createNewTab().getEngine());
     progressLine.visibleProperty().bind(engine.getLoadWorker().runningProperty());
-    progressLine.visibleProperty().addListener((observable, oldValue, newValue) -> {
-      if(newValue){
-        strokeTransition.play();
-      }
-    });
     engine.getLoadWorker().progressProperty().addListener((observable, oldValue, newValue) -> {
       double progress = newValue.doubleValue();
       progressLine.setEndX(stackPane.getWidth() * progress);
+      progressLine.setOpacity(1 - (1 * progress));
     });
+
+    tooltip.textProperty().bind(engine.titleProperty());
     initTabMenu();
   }
 
 
 
-  private void initTabMenu(){
+  private void initTabMenu() {
     newTabItem.setAccelerator(new KeyCharacterCombination("Ctrl+T"));
     newTabItem.setOnAction(event -> getBrowserController().createNewTab());
     closeItem.setAccelerator(new KeyCharacterCombination("Ctrl+W"));
@@ -98,8 +96,11 @@ public class BrowserTab extends Tab {
     closeOtherItem.setOnAction(event -> getBrowserController().closeOtherTab(this));
     reloadItem.setAccelerator(new KeyCharacterCombination("Ctrl+R"));
     reloadItem.setOnAction(event -> engine.reload());
+    copyItem.setOnAction(event -> getBrowserController().createNewTab().go(getCurrentUrl()));
     tabMenu = new ContextMenu();
-    tabMenu.getItems().addAll(newTabItem,new SeparatorMenuItem(), reloadItem, new SeparatorMenuItem(), closeItem, closeOtherItem);
+    tabMenu.getItems()
+        .addAll(newTabItem, new SeparatorMenuItem(), reloadItem,copyItem, new SeparatorMenuItem(), closeItem,
+            closeOtherItem);
     this.setContextMenu(tabMenu);
     this.setClosable(true);
   }
@@ -144,5 +145,21 @@ public class BrowserTab extends Tab {
 
   public void setBrowserController(BrowserController browserController) {
     this.browserController = browserController;
+  }
+
+
+  public void snapshot() {
+    Image image = webView.snapshot(null, null);
+    System.out.println("snapshot");
+    try {
+      ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png",
+          new File("test.png"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void cancelLoad(){
+    engine.getLoadWorker().cancel();
   }
 }
